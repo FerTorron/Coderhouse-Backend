95db8bcc9d16c15caefa2183fcfbc649fc535334
FerTorron <fertorron21@gmail.com>
2023-09-03 18:23:39 +0200

    Subiendo Parte de la Primera Integradora

---
+++ PrimeraIntegradora/src/dao/dbConfig.js
@@ -0,0 +1,8 @@
+import mongoose from "mongoose"
+
+const URI = 'mongodb+srv://CoderUser:asd123@cluster0.yb79iwi.mongodb.net/ecommerce'
+
+await mongoose.connect(URI, {
+    serverSelectionTimeoutMS: 5000,
+})
+console.log("Base de datos conectada....")
